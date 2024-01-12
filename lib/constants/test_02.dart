4b899ac82cab60912fdeda016b13792a3d882c02
krishn.vaibhav <krishnvaibhav.12c1@gmail.com>
2024-01-12 16:51:50 +0530

    New page added

---
+++ lib/constants/test_02.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const key = "key_02";
+const color = Colors.orange;
+const title = "Orange APP";
+
+final Map OObj1 = {'key': key, 'title': title, 'color': color};
---
+++ lib/constants/tet_01.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const key = "key";
+const color = Colors.blue;
+const title = "Barcelona App";
+
+final Map BObj = {'key': key, 'title': title, 'color': color};
